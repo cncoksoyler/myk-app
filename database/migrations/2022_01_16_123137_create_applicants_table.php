037efbc3f110db7e9d984151ff34844eec063eee
cncoksoyler <cncoksoyler@gmail.com>
2022-01-16 21:02:44 +0300

    migrations created

---
+++ database/migrations/2022_01_16_123137_create_applicants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Profession;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('TC');
+            $table->string('mobile');
+            $table->string('workplace');
+            $table->foreignIdFor(Profession::class);
+            $table->string('speciality_detail');
+            $table->string('subspeciality_detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applicants');
+    }
+}
---
+++ database/migrations/2022_01_16_124325_create_results_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Applicant;
+use App\Models\Exam;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Exam::class);
+            $table->foreignIdFor(Applicant::class);
+            $table->timestamps();
+            $table->unique(['exam_id','applicant_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
