1f9ab2638c65b346c210d4518720d4b785be67b1
cncoksoyler <cncoksoyler@gmail.com>
2022-03-10 17:47:45 +0300

    DatabaseSeeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
         $admin = new User();
         $admin->name = 'Admin';
         $admin->email = 'admin@admin.com';
